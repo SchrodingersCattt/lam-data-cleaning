5d726f231bd1b9f5616394614ab360f045133e3c
zjgemi <liuxin_zijian@163.com>
2023-07-04 21:16:07 +0800

    test on validation data
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- dpclean/op/split_dataset.py
+++ dpclean/op/split_dataset.py
@@ -1,44 +1,45 @@
 import os
+import random
 from pathlib import Path
 from typing import List
 
 import dpdata
-from dflow.python import OP, OPIO, Artifact, OPIOSign
+from dflow.python import OP, OPIO, Artifact, OPIOSign, Parameter
 
 
 class SplitDataset(OP):
     @classmethod
     def get_input_sign(cls):
-        return OPIOSign(
-            {
-                "dataset": Artifact(Path),
-            }
-        )
+        return OPIOSign({
+            "dataset": Artifact(Path),
+            "n_init": Parameter(int, default=0),
+        })
 
     @classmethod
     def get_output_sign(cls):
-        return OPIOSign(
-            {
-                "systems": Artifact(List[Path]),
-                "n_total": int,
-            }
-        )
+        return OPIOSign({
+            "systems": Artifact(List[Path]),
+            "init_systems": Artifact(List[Path]),
+            "n_total": int,
+        })
 
     @OP.exec_sign_check
     def execute(self, ip: OPIO) -> OPIO:
         systems = []
-        for f in os.listdir(ip["dataset"]):
-            path = os.path.join(ip["dataset"], f)
-            if os.path.isdir(path):
-                k = dpdata.LabeledSystem(path, fmt="deepmd/npy")
-                nf = k.get_nframes()
+        for f in ip["dataset"].rglob("**/type.raw"):
+            path = f.parent
+            k = dpdata.LabeledSystem(path, fmt="deepmd/npy")
+            nf = k.get_nframes()
 
-                for i in range(nf):
-                    target = "%s.%s" % (f, i)
-                    k[i].to_deepmd_npy_mixed(target)
-                    systems.append(Path(target))
+            for i in range(nf):
+                target = path.relative_to(ip["dataset"]) / str(i)
+                print("Save system to ", target)
+                k[i].to_deepmd_npy_mixed(target)
+                systems.append(target)
 
+        init_systems = random.sample(systems, ip["n_init"])
         return OPIO({
-            "systems": systems,
+            "systems": [s for s in systems if s not in init_systems],
+            "init_systems": init_systems,
             "n_total": len(systems),
         })
