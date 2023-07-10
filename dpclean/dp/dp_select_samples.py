af524fe6a14a4565952a38bda32659410f64c599
zjgemi <liuxin_zijian@163.com>
2023-07-10 08:19:20 +0800

    add train only mode
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- dpclean/dp/dp_select_samples.py
+++ dpclean/dp/dp_select_samples.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from typing import List, Tuple
 
 import numpy as np
-from dpclean.op import SelectSamples
+from dpclean.op import SelectSamples, Validate
 
 
-class DPSelectSamples(SelectSamples):
+class DPValidate(Validate):
     def load_model(self, model: Path):
         from deepmd.infer import DeepPot
         self.dp = DeepPot(model)
@@ -19,3 +19,7 @@ class DPSelectSamples(SelectSamples):
         cell = cell.reshape([1, -1])
         e, f, v = self.dp.eval(coord, cell, atype)
         return e[0], f[0], v[0]
+
+
+class DPSelectSamples(SelectSamples, DPValidate):
+    pass
