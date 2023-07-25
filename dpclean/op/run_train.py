719af4265946afec37d1a1a5b696fec988a45f50
zjgemi <liuxin_zijian@163.com>
2023-07-25 14:13:11 +0800

    add resume lr
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- dpclean/op/run_train.py
+++ dpclean/op/run_train.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import List
 
-from dflow.python import OP, OPIO, Artifact, OPIOSign
+from dflow.python import OP, OPIO, Artifact, OPIOSign, Parameter
 
 
 class RunTrain(OP, ABC):
@@ -15,6 +15,7 @@ class RunTrain(OP, ABC):
                 "finetune_model": Artifact(Path, optional=True),
                 "model": Artifact(Path, optional=True),
                 "train_params": dict,
+                "resume_lr": Parameter(float, default=None),
             }
         )
 
