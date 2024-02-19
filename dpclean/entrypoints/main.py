1abf03929cae54e77046c53f877f0159a0c382a2
zjgemi <liuxin_zijian@163.com>
2024-02-19 12:26:59 +0800

    fix resubmit active learning
    Signed-off-by: zjgemi <liuxin_zijian@163.com>

--- dpclean/entrypoints/main.py
+++ dpclean/entrypoints/main.py
@@ -8,6 +8,7 @@ from dflow import Workflow
 from dpclean.flow import build_workflow
 
 
+BIGNUM = 1000
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
 if log_level:
     logging.basicConfig(level=getattr(logging, log_level.upper(), logging.INFO))
@@ -61,6 +62,26 @@ def parse_args(args: Optional[List[str]] = None):
     return parsed_args
 
 
+def order(key):
+    fields = key.split("-")
+    if key == "train-zero":
+        return -2
+    if key == "valid-zero":
+        return -1
+    if fields[0] == "iter":
+        if fields[-1] == "train":
+            return 2*int(fields[1])
+        if fields[-1] == "select":
+            return 2*int(fields[1])+1
+    if key == "train-all":
+        return 2*BIGNUM
+    if key == "valid-all":
+        return 2*BIGNUM+1
+    if key == "merge":
+        return 2*BIGNUM+2
+    return -3
+
+
 def main():
     args = parse_args()
     if args.command == "submit":
@@ -73,17 +94,7 @@ def main():
         reused_steps = [step for step in wf0.query_step() if step.key is not None and step.phase == "Succeeded"]
         restart_from = getattr(args, "from", None)
         if restart_from is not None:
-            fields = restart_from.split("-")
-            iter = int(fields[1])
-            task = fields[2]
-            if task == "train":
-                reused_steps = [s for s in reused_steps
-                                if (s.key.startswith("iter-") and int(s.key.split("-")[1]) < iter)
-                                or not s.key.startswith("iter-")]
-            elif task == "select":
-                reused_steps = [s for s in reused_steps
-                                if (s.key.startswith("iter-") and int(s.key.split("-")[1]) < iter)
-                                or s.key == "iter-%s-train" % iter or not s.key.startswith("iter-")]
+            reused_steps = [s for s in reused_steps if order(s.key) < order(restart_from)]
         with open(args.CONFIG, "r") as f:
             config = json.load(f)
         wf = build_workflow(config)
